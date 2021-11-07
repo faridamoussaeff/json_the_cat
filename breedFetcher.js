5251ed67ab3bf5b9cc58b90ca31a3032032e990a
Farida <faridamus@protonmail.com>
2021-11-07 01:05:50 +0000

    Update after eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,18 @@
 const request = require('request');
 
-const fetchBreedDescription = function (breedName, callback) {
-    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, response, body) {
-        if (error) {
-            callback(error, null);
-        }
-        
-        const data = JSON.parse(body);
-        if (data[0] === undefined) {
-            callback(null, `${breedName} not found`);
-        } else {
-          callback(null, data[0]['description']);
-        }
-    });
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback(null, `${breedName} not found`);
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
 };
 
 module.exports = { fetchBreedDescription };
